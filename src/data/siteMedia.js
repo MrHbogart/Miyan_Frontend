f429b3fff995bcd8e01d649612b32652f9055200
Hossein <ho.saeidiwork@gmail.com>
2025-11-08 16:05:19 +0330

    correcting some namigs

--- src/data/siteMedia.js
+++ src/data/siteMedia.js
@@ -1,7 +1,7 @@
 import heroImg from '@/assets/videos/hero.mp4';
 import miyanLogo from '@/assets/images/miyan_logo.png';
-import bereshtLogo from '@/assets/images/beresht_logo.png';
-import madiLogo from '@/assets/images/madi_logo.png';
+import bereshtLogo from '@/assets/images/miyan_beresht_logo.png';
+import madiLogo from '@/assets/images/miyan_madi_logo.png';
 
 // Central holder for site-level media paths (videos, hero images, etc.)
 export default {
